54d73f4750a5c4548462ff9b15535510b1ad88b4
samery46 <syamme@ymail.com>
2024-01-11 10:50:43 +0700

    Initialize Project

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="c-footer">
+    <div>Apps © {{ date('Y') }} || Made by <strong><a target="_blank"
+                href="http://ketik-kan.com/">Sam_Ery</a></strong></div>
+
+    <div class="mfs-auto d-md-down-none">Version <strong class="text-danger">1.0</strong></div>
+</footer>
